f81b57a7597716a598bba3295ec8fc141f669fc0
Leo Peng <pengzimaobuaa@163.com>
2016-06-01 11:41:11 +0800

    First commit

---
+++ ControlFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ControlFun
+//
+//  Created by Leo Peng on 6/1/16.
+//  Copyright © 2016 Leo Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ControlFun/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  ControlFun
+//
+//  Created by Leo Peng on 6/1/16.
+//  Copyright © 2016 Leo Peng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)textFieldDoneEditing:(id)sender {
+    [sender resignFirstResponder];
+}
+
+- (IBAction)backgroundTap:(id)sender {
+    [self.nameField resignFirstResponder];
+    [self.numberField resignFirstResponder];
+}
+
+@end
